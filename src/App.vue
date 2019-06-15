77cd041379309b2e17b4d09601c6fd76e100b4be
LuisChen <leo5916267@gmail.com>
2019-06-15 22:31:21 +0800

    完成Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,23 @@
 <template>
-    <v-app>
-        <v-toolbar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Vuetify</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn
-                flat
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-            >
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
-        </v-toolbar>
+  <v-app>
+    <app-navigation></app-navigation>
 
-        <v-content>
-            <HelloWorld />
-        </v-content>
-    </v-app>
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import AppNavigation from './components/AppNavigation';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
-    },
-    data() {
-        return {
-            //
-        };
+        AppNavigation
     }
 };
 </script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-    <HelloWorld />
+  <span>
+    <home-hero></home-hero>
+    <home-details></home-details>
+    <home-plans></home-plans>
+  </span>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails';
+import HomePlans from '@/components/HomePlans';
 
 export default {
+    name: 'home',
     components: {
-        HelloWorld
+        HomeHero,
+        HomeDetails,
+        HomePlans
     }
 };
 </script>
